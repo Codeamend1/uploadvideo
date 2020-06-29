f403a41b38153c5bc4424ebeb24f96fd87dbe15c
Codeamend <67585664+Codeamend1@users.noreply.github.com>
2020-06-29 18:28:58 +0530

    Updated
    Index File Modified

--- index.js
+++ index.js
@@ -1,91 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var flash        = require('req-flash');
+var express = require('express')
+var app = express()
+var port = 5000
+var http = require('http');
+var fs = require('fs');
 
-var apiuserRouter = require('./routes/userapi');
-var apivendorRouter = require('./routes/vendorapi');
+var multer  = require('multer')
+ 
+app.get('/', function (req, res, next) {
+    fs.readFile('form.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+})
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+    filename:function(req,file,cb){
+        cb(null,file.originalname.toLowerCase());
+    }
+})
+ 
+var upload = multer({ storage: storage })
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
-var vendorRouter = require('./routes/vendor');
+app.post('/upload', upload.single('video'), function (req, res, next) {
+   console.log('Video File: '+ req.file.originalname.toLowerCase());
+  try {
+    res.send(req.file);
+  }catch(err) {
+    res.send(400);
+  }
+})
 
-
-var expressValidator = require('express-validator');
-
-var connection  = require('express-myconnection'); 
-var mysql = require('mysql');
-
-var app = express();
-var reload = require('reload');
-
-app.set('port', 7777);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({secret:"webnexs"}));
-app.use(expressValidator());
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
-app.use(
-    connection(mysql,{
-        host: 'localhost',
-        user: 'root', 
-        password : '', 
-        port : 3306, 
-        database:'wcart' 
-    },'pool')
-);
-app.use(flash());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
-app.use('/vendor', vendorRouter);
-
-app.use('/userapi', apiuserRouter);
-app.use('/vendorapi', apivendorRouter);
-
-app.use(function(req, res, next) {
-    req.header("Content-Type", "application/json");
-    next();
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-/* catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});*/
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-    
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(app.get('port'));
-console.log('Server Runnign on port localhost:' + app.get('port'));
-
-module.exports = app;
+app.listen(port, () => console.log(`Video Upload Form at http://localhost:${port}`))
